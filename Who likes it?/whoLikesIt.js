6d59e50b67b0090dff7ec7cff2843ba69f3bc15f
Gema Martin <geminway@live.com>
2022-01-29 11:43:36 +0100

    First test whoLiketIt

--- Who likes it?/whoLikesIt.js
+++ Who likes it?/whoLikesIt.js
@@ -1,5 +1,17 @@
-function sum(a, b) {
-  return a + b;
+function whoLikeIt(arrayPost) {
+  if(arrayPost.length <= 0 ){
+    return "no one likes this";
+  }else if(arrayPost.length === 1){
+    return `${arrayPost[0]} likes this`;
+  }else if(arrayPost.length === 3){
+    return  `${arrayPost[0]}, ${arrayPost[1]} and ${arrayPost[2]} like this`
+  }else if(arrayPost.length >= 4){
+    const numberLikePeople = arrayPost.length - 2
+
+    return `${arrayPost[0]}, ${arrayPost[1]} and ${numberLikePeople} others like this`
+  }
+
 }
-module.exports = sum;
+
+module.exports = whoLikeIt;
 
--- Who likes it?/whoLikesIt.test.js
+++ Who likes it?/whoLikesIt.test.js
@@ -1,6 +1,20 @@
-const sum = require('./whoLikesIt');
+const whoLikeIt = require('./whoLikesIt');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+describe('WhoLikeIt.js',() => {
+  test('Debe el array inicializar vacio', () => {
+    expect(whoLikeIt([])).toBe("no one likes this");
+  });
+
+  test('Si una persona le dio a me gusta debe retornar...', () => {
+    expect(whoLikeIt(['Peter'])).toBe("Peter likes this");
+  });
+
+  test('Si tres personas le han dado a me gusta debe retornar', () => {
+    expect(whoLikeIt(["Max", "John", "Mark"])).toBe("Max, John and Mark like this");
+  });
+
+  test('Si son 4 o más personas debe retornar...', () => {
+    expect(whoLikeIt(["Alex", "Jacob", "Mark", "Max"])).toBe("Alex, Jacob and 2 others like this");
+  });
+})
 
